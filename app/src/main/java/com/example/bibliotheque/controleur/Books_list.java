6a93324a56abda91aec4df076b1a19ee541f13c3
justinbah <bahier.justin@gmail.com>
2021-03-31 13:47:13 +0200

    ajout bouton vers menu

--- app/src/main/java/com/example/bibliotheque/controleur/Books_list.java
+++ app/src/main/java/com/example/bibliotheque/controleur/Books_list.java
@@ -2,7 +2,9 @@ package com.example.bibliotheque.controleur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bibliotheque.R;
 
@@ -12,5 +14,10 @@ public class Books_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_list);
+    }
+    public void Menu(View view) {
+
+        Intent intent = new Intent(this, Admin.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bibliotheque/controleur/User_form.java
+++ app/src/main/java/com/example/bibliotheque/controleur/User_form.java
@@ -2,7 +2,9 @@ package com.example.bibliotheque.controleur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bibliotheque.R;
 
@@ -12,5 +14,10 @@ public class User_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_form);
+    }
+    public void Menu(View view) {
+
+        Intent intent = new Intent(this, Admin.class);
+        startActivity(intent);
     }
 }
